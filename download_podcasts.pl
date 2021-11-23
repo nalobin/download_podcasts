d965ce098ffd2c9634fd6300470297909c13de40
Александр Налобин <nalobin@reg.ru>
2021-11-23 10:37:50 +0500

    Cahnge radio-t rss feed to feed with all arvhives

--- download_podcasts.pl
+++ download_podcasts.pl
@@ -15,7 +15,7 @@ $ENV{MOJO_MAX_MESSAGE_SIZE} = 200_000_000;
 
 my %podcasts = (
     the_art    => 'http://taop.rpod.ru/rss.xml',
-    radio_t    => 'http://feeds.rucast.net/radio-t',
+    radio_t    => 'https://radio-t.com/podcast-archives.rss', # 'http://feeds.rucast.net/radio-t',
     it_compot  => 'http://hack.podfm.ru/it_compot/rss/rss.xml',
     makeitsexy => 'http://makeitsexy.rpod.ru/rss.xml',
     radioma    => 'http://radioma.org/feed',
